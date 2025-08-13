96b1ee285867f42d907a7b508c41f09cc3c2cfe3
Liesl-source <tezvarghese@gmail.com>
2025-08-13 12:01:27 +0530

    Functions

---
+++ PYTHON/string.py
@@ -0,0 +1,13 @@
+#lowerfunction
+text="Hello World!"
+print(text.upper())
+word=["Python" ,"Is" ,"Fun!"]
+print(" ".join (word))
+print("-".join(word))
+#splitfuction
+A="I Love Python Programming!"
+result=A.split("-")
+print (result)
+#findfuction
+B="Python Programming"
+print(B.find("Progr"))
