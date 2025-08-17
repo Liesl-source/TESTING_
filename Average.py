de6c34cde39fa644c9ca7c947949a66d7ce97dc5
Liesl-source <tezvarghese@gmail.com>
2025-08-17 12:24:12 +0530

    Averages

---
+++ Average.py
@@ -0,0 +1,7 @@
+tree1=30
+tree2=43
+tree3=29
+tree4=45
+tree5=33
+Sum=(tree1+tree2+tree3+tree4+tree5)/5
+print(Sum)
---
+++ PYTHON/Congrats.py
@@ -0,0 +1,9 @@
+#Lowerfunction
+text="Congratulations"
+print(text.lower())  
+print(text.upper())
+print("Congrats on winning a gold medal!".upper()) 
+print("Congrats on winning a gold medal!".lower())
+Word=["Congrats" , "on" , "winning" , "a" , "gold" , "medal!"]
+result="-".join (Word)
+print(result.upper())
