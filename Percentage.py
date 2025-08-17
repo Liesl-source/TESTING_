bdd421ec97658ea6758892948f3f29d177378da5
Liesl-source <tezvarghese@gmail.com>
2025-08-17 13:01:06 +0530

    Percentages

---
+++ Percentage.py
@@ -0,0 +1,6 @@
+Maths=int(input("Maths:"))
+English=int(input("English:"))
+Hindi=int(input("Hindi:"))
+Science=int(input("Science:"))
+Percentage=((Science+Hindi+English+Maths)/400)*100
+print("Raj got",Percentage,"%")
