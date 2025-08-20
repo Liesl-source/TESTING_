b18d53691484c471e363b514a46659cde38abc5b
Liesl-source <tezvarghese@gmail.com>
2025-08-20 12:02:09 +0530

    Print Statements

---
+++ PYTHON/Print_Statements.py
@@ -0,0 +1,8 @@
+I=int(input("Please enter a value :)"))
+if I>15:
+    print(f"{I} is greater than 15 yay!!!")
+    print("im in the if block")
+else:
+    print("Your number is NOT greater than 15 :(")
+    print("im in the else block")
+print("in not in the else nor if block")
