9a47810cd3528d7576c1b8eead5a0ad578488283
Liesl-source <tezvarghese@gmail.com>
2025-08-20 12:20:39 +0530

    odd or even?

---
+++ PYTHON/odd_or_even.py
@@ -0,0 +1,5 @@
+number=int(input("Please enter a number!"))
+if number % 2 ==0:
+    print(f"YAY!!! {number} is an even number!!!")
+else:
+    print(f"Aww :( {number} is not an even number....it's odd...")
