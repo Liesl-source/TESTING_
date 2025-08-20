0ece6cfc739d1f70a32eaddb4077140221642d72
Liesl-source <tezvarghese@gmail.com>
2025-08-20 11:39:12 +0530

    If_Statement

---
+++ PYTHON/If_Statement.py
@@ -0,0 +1,8 @@
+number=3
+if number<10:
+    print("This number is smaller than 10")
+number=3
+if number>10:
+    print("This number is greater than 10")
+else:
+    print("This number is smaller than 10")
---
+++ PYTHON/Square_root.py
@@ -0,0 +1,9 @@
+"""import math
+number= int(input("Enter a number!")) 
+square_root= math.sqrt(number)
+print ("The square root of",number, "is:",square_root)
+print(f"The square root of {number} is: {square_root}")"""
+#Usingexpontiation
+Answer= float(input("Enter a number!"))
+sqrt= Answer**0.5
+print(f"THe square root of {Answer} is: {sqrt}.")
