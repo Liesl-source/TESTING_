2b951083739c1d11935625b69887255fd7972c10
Liesl-source <tezvarghese@gmail.com>
2025-08-22 10:22:51 +0530

    Not_Operator

---
+++ PYTHON/Not_Operator.py
@@ -0,0 +1,21 @@
+#NOT_LOGICAL_OPERATOR
+a=13
+b=20
+c=20
+print(a!= b)
+print(b!= c)
+
+A="Python"
+B="Coding"
+if A != B:
+    print(f"{A} and {B} are different words!")
+
+a=2
+b=4
+if (a==2) != (b==4):
+    print("Hello!")
+a= int(input("Please enter a number!"))
+if a%2 !=0:
+    print(f"{a} is an odd number!!!")
+else:
+    print(f"{a} is an even number!!!")
