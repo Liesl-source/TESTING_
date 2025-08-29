4df7d42ef885b3c09106e0ae806c70e7a25423ed
Liesl-source <tezvarghese@gmail.com>
2025-08-29 10:10:59 +0530

    Divisble

---
+++ Divisible.py
@@ -0,0 +1,8 @@
+print("Please enter Numerator:")
+numm= int(input())
+print("Please enter Denominator:")
+numd= int(input())
+if numm%numd==0:
+    print(str(numm)+" Is divisable by "+str(numd))
+else:
+    print(str(numm)+" Is not divisable by 7"+str(numd))
