4cfa190736a1d18df380a6e45c7546b890d926e0
Liesl-source <tezvarghese@gmail.com>
2025-08-29 10:21:57 +0530

    Mean

---
+++ Mean.py
@@ -0,0 +1,8 @@
+mean1=36
+wrongnum=38
+correctnum=56
+totalnum=40
+sum=mean1*totalnum
+print("The sum of 40 is:",sum)
+sum2=sum-((wrongnum)-(correctnum))
+print("the sum of 40 minus 38 and 52 is:",sum2)
