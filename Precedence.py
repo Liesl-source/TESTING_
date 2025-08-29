05d36a45fae6735fd7891fa17f6a9725816658b2
Liesl-source <tezvarghese@gmail.com>
2025-08-29 09:59:17 +0530

    Precedence

---
+++ Precedence.py
@@ -0,0 +1,14 @@
+v=8
+w=4
+x=4
+y=6
+z=9
+z=(v+w)* x/y
+print("(V+W)* X/Y =",z)
+
+name="Alex"
+age=0
+if (name=="Alex") or ((name=="Ella") and (age>=2)):
+    print("Hello World")
+else:
+    print("Goodbye")
