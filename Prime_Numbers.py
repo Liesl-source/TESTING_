97f2fcfb3dfb8936d532815cd0745a44437e60ed
Liesl-source <tezvarghese@gmail.com>
2025-09-12 10:30:15 +0530

    Prime Nunbers in Range

---
+++ Prime_Numbers.py
@@ -0,0 +1,10 @@
+low=int(input("Enter the lower range:"))
+up=int(input("Enter the upper range:"))
+print("Prime numbers between",low,"and",up,"are:")
+for i in range(low,up+1):
+    if i>1:
+        for j in range(2,i):
+            if(i%j==0):
+                break
+        else:
+            print(i)
