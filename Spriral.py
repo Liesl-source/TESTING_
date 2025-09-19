cf4fe9d518907c14266de8e4d6da384e1d3fe7a9
Liesl-source <tezvarghese@gmail.com>
2025-09-19 09:49:35 +0530

    Spirals (Infinite Loop)

---
+++ Spriral.py
@@ -0,0 +1,17 @@
+import turtle
+spiral=turtle.Screen()
+spiral.bgcolor("lightblue")
+spiral.title("Turtle")
+pen=turtle.Turtle()
+size=0
+while True:
+    for i in range(4):
+        pen.fd(size+1)
+        pen.left(90)
+        size-=5
+    size=size+1
+    if size
+
+
+    
+
