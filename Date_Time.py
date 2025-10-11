a58dd15d9be4d964f17070a84eed28b1673dfcae
Liesl-source <tezvarghese@gmail.com>
2025-10-11 14:16:15 +0530

    Date and Time module

---
+++ Date_Time.py
@@ -0,0 +1,8 @@
+from datetime import date, time, datetime
+today= date.today()
+now= datetime.now()
+#date and time
+print("Today's date is", today)
+print("\nCurrent date and time is",now)
+#Dates components
+print("n\Date components",today.year,today.month,today.day)
