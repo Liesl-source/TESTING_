08c069cdf278ee511b15a982049c276ca60e01fc
Liesl-source <tezvarghese@gmail.com>
2025-10-15 11:24:05 +0530

    Lists

---
+++ Lists.py
@@ -0,0 +1,12 @@
+empty_list=[]
+print()
+#a list of numbers
+numbers=[1,2,3,4,5]
+print(numbers)
+#try using the * operator
+triples=[1,2,3]*3
+print(triples)
+#Reversing a list
+Rev_list=[100,200,300,400,500]
+Rev_list=Rev_list[::-1]
+print(Rev_list,"\n")
