fd43a9d76a1d8755c2e4eba81a09b59058e313e2
Tesy Varghese <tezvarghese@gmail.com>
2025-12-10 11:25:10 +0530

    Sets

---
+++ Sets.py
@@ -0,0 +1,18 @@
+set_1={1,2,3}
+print(set_1)
+
+set_2={2.4,"Hello",(1,2,3)}
+print(set_2)
+
+set_3={1,2,3,4,2,3}
+print(set_3)
+
+set_4=set([1,2,3,2])
+print(set_4,"\n")
+
+num_set=set([0,1,3,4,5])
+print("Original Set:")
+print(num_set)
+num_set.pop()
+print("After removing the first element from said set:")
+print(num_set,"\n")
