97e7ebb8b9b4aa37fc578371d7b681bcbb942635
Tesy Varghese <tezvarghese@gmail.com>
2025-12-17 12:02:54 +0530

    SETs and ARRAYs HW

---
+++ Hw.py
@@ -0,0 +1,9 @@
+# Define two sets
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Find symmetric difference
+sym_diff = set1.symmetric_difference(set2)
+
+# Display result
+print("Symmetric Difference:", sym_diff)
