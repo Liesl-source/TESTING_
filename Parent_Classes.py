5b5bed82ea9cf4135f957aa4b2659d41f4107e03
Tesy Varghese <tezvarghese@gmail.com>
2026-01-28 11:40:25 +0530

    Parent classes and Child classes

---
+++ Parent_Classes.py
@@ -0,0 +1,25 @@
+# parent class
+class Person(object):
+   
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+
+# child class
+class Employee(Person):
+    def __init__(self, name, idnumber,  salary, post):
+        
+        self.salary = salary
+        self.post = post
+
+        Person.__init__(self, name, idnumber)
+
+
+emp = Employee("Rahul", 886012, 200000, "Intern")
+emp.display()
+
